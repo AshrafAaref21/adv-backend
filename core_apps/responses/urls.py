6730db96dc048466cc7b6205980ae20a8355f6eb
unknown <ashrafaaref2020@gmail.com>
2024-07-22 12:13:41 +0300

    user can add update remove responses.

---
+++ core_apps/responses/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from .views import ResponseListCreateView, ResposeUpdateDeleteView
+
+
+urlpatterns = [
+    path("article/<uuid:article_id>/", ResponseListCreateView.as_view(), name="article-responses"),
+    path("<uuid:id>/", ResposeUpdateDeleteView.as_view(), name="detail-response"),
+]
---
+++ core_apps/responses/views.py
@@ -0,0 +1,55 @@
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+    get_object_or_404,
+)
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Article, Response
+from .serializers import ResponseSerializer
+
+
+class ResponseListCreateView(ListCreateAPIView):
+    """GET|POST."""
+
+    queryset = Response.objects.all()
+    serializer_class = ResponseSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        article_id = self.kwargs.get("article_id")
+        return Response.objects.filter(article__id=article_id, parent_response=None)
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        article_id = self.kwargs.get("article_id")
+        article = get_object_or_404(Article, id=article_id)
+
+        serializer.save(user=user, article=article)
+
+
+class ResposeUpdateDeleteView(RetrieveUpdateDestroyAPIView):
+    queryset = Response.objects.all()
+    serializer_class = ResponseSerializer
+    lookup_field = "id"
+
+    def perform_update(self, serializer):
+        user = self.request.user
+        response = self.get_object()
+
+        if response.user != user:
+            raise PermissionDenied("You do not have permission to edit this response.")
+
+        serializer.save()
+
+    def perform_destroy(self, instance):
+        user = self.request.user
+        response = self.get_object()
+
+        if response.user != user:
+            raise PermissionDenied(
+                "You do not have permission to delete this response."
+            )
+
+        instance.delete()
